97e5da9faa4e883d5038045dc83aa17804ee0110
Nin <nintran1995@gmail.com>
2017-12-09 22:26:55 +0700

    App new project

---
+++ WorkEverything/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkEverything.Startup))]
+namespace WorkEverything
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WorkEverything/ViewModel/User/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WorkEverything.ViewModel.User
+{
+    public class UserModel
+    {
+        [Display(Name = "Tên đăng nhập")]
+        [Required(ErrorMessage = "Bạn phải nhập tài khoản")]
+        public string userName { get; set; }
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Bạn phải nhập mật khẩu")]
+        public string passWord { get; set; }
+    }
+}
