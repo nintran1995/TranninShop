4e282d6b6efa2047b26ff170a0e66d61f972e105
Nin <nintran1995@gmail.com>
2017-12-15 23:33:53 +0700

    Change Name project

--- WorkEverything/Properties/AssemblyInfo.cs
+++ WorkEverything/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WorkEverything")]
+[assembly: AssemblyTitle("TranninShop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WorkEverything")]
+[assembly: AssemblyProduct("TranninShop")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
