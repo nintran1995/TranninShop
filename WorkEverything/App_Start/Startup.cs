37177c0ac714b2cb1addeaef64941cd9d5a8c8d4
Tansy <tansy@qinfosys.com>
2017-12-15 19:24:32 +0700

    Add icon for the Project

---
+++ WorkEverything/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WorkEverything.App_Start.Startup))]
+
+namespace WorkEverything.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
