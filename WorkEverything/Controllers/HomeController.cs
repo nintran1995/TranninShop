1651c5de4cf65d6ef2c018a8abf84be468d26d09
Nin <nintran1995@gmail.com>
2018-06-07 20:01:34 +0700

    Lession 41

--- WorkEverything/Controllers/HomeController.cs
+++ WorkEverything/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TranninShop.Model.Models;
+using TranninShop.Service;
+using TranninShop.Web.Models;
 
 namespace TranninShop.Web.Controllers
 {
 	public class HomeController : Controller
 	{
+		IProductCategoryService _productCategoryService;
+
+		public HomeController(IProductCategoryService productCategoryService) {
+			this._productCategoryService = productCategoryService;
+		}
+
 		public ActionResult Index()
 		{
 			return View();
@@ -17,6 +27,14 @@ namespace TranninShop.Web.Controllers
 		public ActionResult Header()
 		{
 			return PartialView();
+		}
+
+		[ChildActionOnly]//Chi de nhung
+		public ActionResult Menu()
+		{
+			var model = _productCategoryService.GetAll();
+			var listProductCategoryViewModel = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+			return PartialView(listProductCategoryViewModel);
 		}
 
 		[ChildActionOnly]//Chi de nhung
